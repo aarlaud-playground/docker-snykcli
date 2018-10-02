7972285a705040557b9f3ebfb26d1b1d38f5da2f
Antoine Arlaud <aarlaud@hotmail.com>
2018-10-02 17:16:36 -0400

    fix: cleaning up a bit

--- scripts/snyk-cli.py
+++ scripts/snyk-cli.py
@@ -6,36 +6,42 @@ import sys
 import subprocess
 import tarfile
 
-def main(docker_command):
+def main(docker_image_to_test):
 
-    proc = subprocess.Popen(docker_command, shell=True, stdout=subprocess.PIPE)
-    out, err = proc.communicate()
-    docker_image_id = out.decode("utf-8").strip('\n')
+    # proc = subprocess.Popen(docker_command, shell=True, stdout=subprocess.PIPE)
+    # out, err = proc.communicate()
+    # docker_image_id = out.decode("utf-8").strip('\n')
 
 
     snyk_token = os.getenv('SNYK_TOKEN')
+    snyk_org = os.getenv('SNYK_ORG')
 
-    proc = subprocess.Popen(docker_command, shell=True, stdout=subprocess.PIPE)
-    out, err = proc.communicate()
-    docker_image_id = out.decode("utf-8").strip('\n')
-    print("image")
-    print(docker_image_id)
-    print("end of image")
+    # proc = subprocess.Popen(docker_command, shell=True, stdout=subprocess.PIPE)
+    # out, err = proc.communicate()
+    # docker_image_id = out.decode("utf-8").strip('\n')
+    # print("image")
+    # print(docker_image_id)
+    # print("end of image")
 
-    # Base twistcli commnad to scan images
+    # Base snykcli commnad to scan images
     #snykcli_base_command = '/usr/local/bin/snyk-linux test https://github.com/aarlaud-snyk/github-stats'
-    snykcli_base_command = 'snyk test --docker node:10-alpine --org=aarlaud-snyk-demo'
+    snykcli_base_command = 'snyk test --docker '
+    snykcli_org_option = ''
+    if snyk_org:
+        snykcli_org_option = ' --org='+snyk_org
+
+    # Concatenate snykcli executable with options from pipeline variables
+    snykcli_exec = ' '\
+    .join([snykcli_base_command, docker_image_to_test, snykcli_org_option])
 
     proc = subprocess.Popen(snykcli_base_command, shell=True, stdout=subprocess.PIPE)
     stdout = proc.communicate()[0].decode('utf-8').strip('\n')
 
-    # Concatenate twistcli executable with options from pipeline variables
-    twistcli_exec = ' '\
-        .join([snykcli_base_command, docker_image_id])
-
-    # Execute command pipe stdout to variable and pipe to stdout and use for final exit code for threshold support
-    proc = subprocess.Popen(twistcli_exec, shell=True)
-    proc.communicate()
+    #
+    #
+    # # Execute command pipe stdout to variable and pipe to stdout and use for final exit code for threshold support
+    # proc = subprocess.Popen(snykcli_exec, shell=True)
+    # proc.communicate()
 
     if proc.returncode != 0:
         sys.exit(1)
